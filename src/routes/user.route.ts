e2c8eeb371a336073bedf9702e7ebcb8577f45a0
alexmm23 <alejandromontes393@gmail.com>
2024-02-28 22:00:49 -0600

    Refactor de autenticacion de rutas

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,41 +4,29 @@ import UserService from '../services/user.service'
 import passport from 'passport'
 const router = express.Router()
 const service = new UserService()
+router.post('/', async (req, res) => {
+  const user: User = req.body
+  const newUser = await service.create(user)
+  res.status(201).json(newUser)
+})
 
-router.post(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    const user: User = req.body
-    const newUser = await service.create(user)
-    res.status(201).json(newUser)
+router.use(passport.authenticate('jwt', { session: false }))
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await service.findAll()
+    res.status(200).json(users)
+  } catch (error) {
+    next(error)
   }
-)
+})
 
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res, next) => {
-    try {
-      const users = await service.findAll()
-      res.status(200).json(users)
-    } catch (error) {
-      next(error)
-    }
+router.get('/:email', async (req, res, next) => {
+  try {
+    const user = await service.findByEmail(req.params.email)
+    res.status(200).json(user)
+  } catch (error) {
+    next(error)
   }
-)
-
-router.get(
-  '/:email',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res, next) => {
-    try {
-      const user = await service.findByEmail(req.params.email)
-      res.status(200).json(user)
-    } catch (error) {
-      next(error)
-    }
-  }
-)
+})
 
 export default router
